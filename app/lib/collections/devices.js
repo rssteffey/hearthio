7ecf4d66430fe63463510e37c445e9a1ef9fb73f
Justin Carter <justincarterdev@gmail.com>
2016-02-04 02:50:24 +0000

    Added iron folder for running iron commands and tested by making devices collection

---
+++ app/lib/collections/devices.js
@@ -0,0 +1,32 @@
+Devices = new Mongo.Collection('devices');
+
+
+if (Meteor.isServer) {
+  Devices.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Devices.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
