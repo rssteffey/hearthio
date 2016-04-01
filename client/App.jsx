1ff644ab0d3f3984f8d46d9dc0bca6bf8c187b88
rssteffey <Shawn@Borealis.local>
2016-04-01 07:15:06 -0500

    Broke things
    I broke stuff

--- client/App.jsx
+++ client/App.jsx
@@ -3,6 +3,9 @@ App = React.createClass({
   render: function() {
     return (
       <div className="container">
+      	<div id="house-panel">
+      		<House_Panel />
+      	</div>
         <div id="widgets-panel">
           <Widgets_Panel />
         </div>
