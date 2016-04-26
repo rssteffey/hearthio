9d0feb2f38837fa3f2247912b843b1205bc19b19
rssteffey <Shawn@Borealis.local>
2016-04-26 14:44:25 -0500

    Changing to JS importer format
    Switching up how our third party libraries load to keep main.js clean

--- client/main.js
+++ client/main.js
@@ -1,9 +1,14 @@
+
+//Architecture imports
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-//import '../imports/startup/accounts-config.js';
+import '../imports/startup/client/index.js'
+
+//Our imports
 import App from '../imports/ui/components/App.js';
+
 
 Meteor.startup(() => {
     render(<App />, document.getElementById('render-target'));
---
+++ imports/startup/client/index.js
@@ -0,0 +1,7 @@
+import { panzoom } from 'jquery.panzoom';
+
+
+//Allow panning of background image
+$(document).ready(function() {
+	$("#pan_and_scan").panzoom();
+});
